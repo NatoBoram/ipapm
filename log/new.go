74cf18ee6451e0c5c34ad5ef88d8111b8ac54d67
Nato Boram <nato.boram+git@gmail.com>
2026-08-06 03:19:44 -0400

    🔊 Colour some logs
    Signed-off-by: Nato Boram <nato.boram+git@gmail.com>

--- log/new.go
+++ log/new.go
@@ -3,6 +3,7 @@ package log
 import (
 	"log/slog"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/NatoBoram/ipapm/env"
@@ -34,6 +35,8 @@ func handler(c Config) slog.Handler {
 }
 
 var (
+	nord04 = gchalk.Hex(nord.Nord04.String())
+	nord06 = gchalk.Hex(nord.Nord06.String())
 	nord11 = gchalk.Hex(nord.Nord11.String())
 	nord13 = gchalk.Hex(nord.Nord13.String())
 	nord14 = gchalk.Hex(nord.Nord14.String())
@@ -41,20 +44,64 @@ var (
 )
 
 func replace(groups []string, attr slog.Attr) slog.Attr {
-	if attr.Key != slog.LevelKey {
+	if attr.Key == slog.LevelKey {
+		switch attr.Value.Any().(slog.Level) {
+		case slog.LevelDebug:
+			attr.Value = slog.StringValue(nord15("DEBUG"))
+		case slog.LevelInfo:
+			attr.Value = slog.StringValue(nord14("INFO"))
+		case slog.LevelWarn:
+			attr.Value = slog.StringValue(nord13("WARN"))
+		case slog.LevelError:
+			attr.Value = slog.StringValue(nord11("ERROR"))
+		}
+
 		return attr
 	}
 
-	switch attr.Value.Any().(slog.Level) {
-	case slog.LevelDebug:
-		attr.Value = slog.StringValue(nord15("DEBUG"))
-	case slog.LevelInfo:
-		attr.Value = slog.StringValue(nord14("INFO"))
-	case slog.LevelWarn:
-		attr.Value = slog.StringValue(nord13("WARN"))
-	case slog.LevelError:
-		attr.Value = slog.StringValue(nord11("ERROR"))
+	// Strings
+	if attr.Key != slog.MessageKey && attr.Value.Kind() == slog.KindString {
+		if value := attr.Value.String(); value != "" {
+			attr.Value = slog.StringValue(nord04(value))
+			return attr
+		}
 	}
+
+	// Message
+	if attr.Key == slog.MessageKey && attr.Value.Kind() == slog.KindString {
+		if value := attr.Value.String(); value != "" {
+			attr.Value = slog.StringValue(nord06(value))
+			return attr
+		}
+	}
+
+	// Errors
+	if attr.Value.Kind() == slog.KindAny {
+		if value, ok := attr.Value.Any().(error); ok {
+			attr.Value = slog.StringValue(nord11(value.Error()))
+			return attr
+		}
+	}
+
+	// Numbers
+	switch attr.Value.Kind() {
+	case slog.KindFloat64:
+		value := strconv.FormatFloat(attr.Value.Float64(), 'f', -1, 64)
+		attr.Value = slog.StringValue(nord15(value))
+		return attr
+
+	case slog.KindInt64:
+		value := strconv.FormatInt(attr.Value.Int64(), 10)
+		attr.Value = slog.StringValue(nord15(value))
+		return attr
+
+	case slog.KindUint64:
+		value := strconv.FormatUint(attr.Value.Uint64(), 10)
+		attr.Value = slog.StringValue(nord15(value))
+		return attr
+
+	}
+
 	return attr
 }
 
